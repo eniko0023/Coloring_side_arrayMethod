82e755a4f4f6f5c475eba21f80ab1a8796f0dbeb
eniko0023 <kisbenedekeniko01@gmail.com>
2024-09-10 13:28:49 +0200

    coloring kesz

--- js/app.js
+++ js/app.js
@@ -48,16 +48,93 @@ const renderBoxes = () => {
 //const boxes = document.querySelectorAll(".box"); 
 //console.log("Boxlista1: ",boxes); //log
 
+//tennivalók: 
+//1. kivenni az input mező értékét
+const getInputValue = function(){
+    return document.querySelector("#num").value;
+};
+
+//2. megfelelő e az érték? (nem üres, nem string, 1 és 15 között van)
+const checkValue = () =>{
+    const value = getInputValue();
+    if (!value.trim()){
+        return [false, 0];
+    }
+    if (isNaN(value)){
+        return [false, 0]; 
+    }
+    const currentValue = Number(value)
+    if(currentValue < 1 || currentValue > 15){
+        return [false, 0]; 
+    }
+    return [true, currentValue];
+};
+
+//4. vétetlen szám generátor
+const randomNumber = () =>{
+    return Math.floor(Math.random() * 256);
+};
+
+//5. számokból színt készíteni
+const createColor = () =>{
+    const r = randomNumber();
+    const g = randomNumber();
+    const b = randomNumber();
+    return [r, g, b];
+};
+
+//szinezes vegrehajtasa
+const coloringBox = () =>{
+    const [ isValid, number ] = checkValue();
+    if (!isValid){
+        sendErrorMessage();
+        return;
+    }
+    const boxes = document.querySelectorAll(".box"); 
+    const box = Array.from(boxes).find(b => Number(b.id) === number);
+    const [ r, g, b ] = createColor();
+    box.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+};
+
+function sendErrorMessage() {
+    alert("Helytelen értéket adott meg!")
+};
+
+function clearInput(){
+    const inputElement = document.querySelector("#num");
+    inputElement.value = "";
+    inputElement.focus();
+}
+
+//szinezo gomb mukodtetese
+const coloring = () =>{
+    const button = document.querySelector(".card button:nth-child(3)");
+    button.addEventListener("click", ()=>{
+        coloringBox();
+        clearInput();
+    });
+   
+};
+
+const reset = () =>{
+    const resetButton = document.querySelector(".card button:nth-child(4)");
+    resetButton.addEventListener("click", () =>{
+        renderBoxes();
+        //coloring();
+        clearInput();
+    })
+}
+
+//3. a színező gombra eseményfigyelőt helyezni - színező callback-eljárás
+
+//6. 3. feladat színező eljárásában alkalmazni ezt a színt
+
+//az oldal inicializalasa - dinamikus tartalom feltoltese
 document.addEventListener("DOMContentLoaded", () => {
     renderBoxes();
+    coloring();
+    reset();
     const boxes = document.querySelectorAll(".box"); 
     console.log("Boxlista3: ",boxes); //log
 });
 
-//tennivalók: 
-//1. kivenni az input mező értékét
-//2. megfelelő e az érték? (nem üres, nem string, 1 és 15 között van)
-//3. a színező gombra eseményfigyelőt helyezni - színező callback-eljárás
-//4. vétetlen szám generátor
-//5. számokból színt készíteni
-//6. 3. feladat színező eljárásában alkalmazni ezt a színt
